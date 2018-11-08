1b9d3d2d573ceee8f660c1998eb2f5ce30071d7f
Daniel de Cloet <djdecloet@gmail.com>
2018-11-08 15:08:33 +0100

    asteroid updates

--- build/app.js
+++ build/app.js
@@ -1,18 +1,45 @@
+const mapSize = [800, 600];
 class Ship {
-    constructor(name, color, cannons, distanceTraveled) {
+    constructor(name, color, cannons) {
         this.name = name;
         this.cannons = cannons;
-        this.distanceTraveled = distanceTraveled;
+        this.distanceTraveled = 0;
+    }
+    addDistanceTraveled() {
+        this.distanceTraveled++;
+    }
+    setCannons(amount) {
+        this.cannons = amount;
     }
     shoot() {
         console.log("I'm shooting");
     }
     move() {
-        this.distanceTraveled++;
+        this.addDistanceTraveled();
         console.log(this.distanceTraveled);
     }
 }
-let spaceShip = new Ship("Nebuchadnezzar", "Gray", 35, 0);
+class Asteroid {
+    constructor(size, location, velocity, rotation, rotationVelocity) {
+        this.size = size;
+        this.location = location;
+        this.velocity = velocity;
+        this.rotation = rotation;
+        this.rotationVeloicty = rotationVelocity;
+    }
+    move() {
+        this.location.map((e, i) => e + this.velocity[i]);
+        this.location.map((e, i) => {
+            if (e < 0)
+                return mapSize[i];
+            if (e > mapSize[i])
+                return 0;
+            return e;
+        });
+        this.rotation += this.rotationVeloicty;
+    }
+}
+let spaceShip = new Ship("Nebuchadnezzar", "Gray", 35);
 spaceShip.move();
 spaceShip.move();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+const mapSize = [800, 600];
+
 class Ship {
     private name: string;
     public color: string;
@@ -7,13 +9,13 @@ class Ship {
     public constructor(
         name: string, 
         color: string, 
-        cannons: number, 
+        cannons: number,
     ) {
         this.name = name;
         this.cannons = cannons;
         this.distanceTraveled = 0;
     }
-    
+
     private addDistanceTraveled() {
         this.distanceTraveled++;
     }
@@ -32,7 +34,43 @@ class Ship {
     }
 }
 
-let spaceShip: Ship = new Ship("Nebuchadnezzar", "Gray", 35, 0);
+class Asteroid {
+    private size: number;
+    private location: number[];
+    private velocity: number[];
+    private rotation: number;
+    private rotationVeloicty: number;
+
+    public constructor(
+        size: number,
+        location: number[],
+        velocity: number[],
+        rotation: number,
+        rotationVelocity: number,    
+    ) {
+        this.size = size;
+        this.location = location;
+        this.velocity = velocity;
+        this.rotation = rotation;
+        this.rotationVeloicty = rotationVelocity;
+    }
+
+    public move() {
+        // add velocity to location
+        this.location.map((e, i) => e + this.velocity[i]);
+        // if location is outside of map, wrap
+        this.location.map((e, i) => {
+            if (e < 0)
+                return mapSize[i];
+            if (e > mapSize[i])
+                return 0;
+            return e;
+        });
+        this.rotation += this.rotationVeloicty;
+    }
+}
+
+let spaceShip: Ship = new Ship("Nebuchadnezzar", "Gray", 35);
 
 spaceShip.move();
 spaceShip.move();
